b999ed1b2fc0fc45f95371f1e6176817fc9388be
geass-zero <nyanfinance@gmail.com>
2020-09-01 16:32:22 -0400

    Updates

---
+++ precache-manifest.4c69f3d9834b82df5ce5a41f87362275.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909c2997265228a97653d50f7652fb97",
+    "url": "/nyan.finance/index.html"
+  },
+  {
+    "revision": "bc0a022f97b12589512e",
+    "url": "/nyan.finance/static/css/main.1130b721.chunk.css"
+  },
+  {
+    "revision": "b86cdf934434783e9170",
+    "url": "/nyan.finance/static/js/2.34badee0.chunk.js"
+  },
+  {
+    "revision": "bc0a022f97b12589512e",
+    "url": "/nyan.finance/static/js/main.584d1f8e.chunk.js"
+  },
+  {
+    "revision": "8c8284dc9083e6624b4b",
+    "url": "/nyan.finance/static/js/runtime-main.477c8fef.js"
+  },
+  {
+    "revision": "ddffeeccd93bfa8838233c4c19073785",
+    "url": "/nyan.finance/static/media/Nyan_pump.ddffeecc.gif"
+  },
+  {
+    "revision": "decb4b682f27410f68bed3cfd25567db",
+    "url": "/nyan.finance/static/media/Nyan_stake.decb4b68.gif"
+  },
+  {
+    "revision": "38ec77e47a3a2a27bced6860ae1252e6",
+    "url": "/nyan.finance/static/media/nyan-small.38ec77e4.gif"
+  },
+  {
+    "revision": "a823024f323f3cd4345044aa8c51d5ac",
+    "url": "/nyan.finance/static/media/telegram.a823024f.png"
+  }
+]);
