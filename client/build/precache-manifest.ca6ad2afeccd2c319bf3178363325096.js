58337f4a63932eaa5c78d28b62596942903c05d2
geass-zero <nyanfinance@gmail.com>
2020-09-10 17:17:58 -0400

    darkNyan UI updates

---
+++ client/build/precache-manifest.ca6ad2afeccd2c319bf3178363325096.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce1df530473c8815ad937afbbc25344",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e6217844a64fc6a94bb",
+    "url": "/static/css/main.24d00c12.chunk.css"
+  },
+  {
+    "revision": "398d87e52092572403d4",
+    "url": "/static/js/2.61f1161b.chunk.js"
+  },
+  {
+    "revision": "9e6217844a64fc6a94bb",
+    "url": "/static/js/main.cbed6335.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "db3931db6426399b5cb94046e9150cb2",
+    "url": "/static/media/Dark_Nyan.db3931db.gif"
+  },
+  {
+    "revision": "ddffeeccd93bfa8838233c4c19073785",
+    "url": "/static/media/Nyan_pump.ddffeecc.gif"
+  },
+  {
+    "revision": "decb4b682f27410f68bed3cfd25567db",
+    "url": "/static/media/Nyan_stake.decb4b68.gif"
+  },
+  {
+    "revision": "1859696178ada5cf499e4b26a62d0597",
+    "url": "/static/media/Nyan_vote.18596961.gif"
+  },
+  {
+    "revision": "011211f0eb123260d8750de3033d968c",
+    "url": "/static/media/catnip.011211f0.png"
+  },
+  {
+    "revision": "99cb2cd3ece198973f5c5b8124512cfb",
+    "url": "/static/media/eth.99cb2cd3.png"
+  },
+  {
+    "revision": "38ec77e47a3a2a27bced6860ae1252e6",
+    "url": "/static/media/nyan-small.38ec77e4.gif"
+  },
+  {
+    "revision": "a823024f323f3cd4345044aa8c51d5ac",
+    "url": "/static/media/telegram.a823024f.png"
+  }
+]);
