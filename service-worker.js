da2542059d7c546a0eaf125fcb2ad7bfa2e2f737
geass-zero <nyanfinance@gmail.com>
2020-09-26 10:04:02 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a44cac47e953e538730aded412c7ca24.js"
+  "/precache-manifest.ad21e183262f0f94f2a287624d0a600d.js"
 );
 
 self.addEventListener('message', (event) => {
